8d4ea139fa9a61fa110c80861b436b2b2f77ccf9
mia-krause <mia.elisabeth.krause+github@gmail.com>
2022-12-03 10:00:38 +0100

    day3

--- src/main/scala/adventofcode/Day3.scala
+++ src/main/scala/adventofcode/Day3.scala
@@ -5,11 +5,27 @@ import scala.reflect.ClassTag
 
 object Day3 {
   val day = this.getClass.getSimpleName.replace("Day", "").replace("$", "")
-  val example = true
+  val example = false
 
   def main(args: Array[String]): Unit = {
     val fileInput = readFile()
+    val overlappingItems = fileInput.map(str => str.splitAt(str.length / 2)).map(evaluateRucksacks1)
+    val badges = fileInput.sliding(3, 3).map(evaluateRucksacks2)
+
+    println(badges.map(toValue).sum)
     }
+
+  def toValue(char:Char) =  if char.isUpper then char - 'A' + 27 else char - 'a' + 1
+
+  def evaluateRucksacks1(rucksacks: (String, String)): Char = {
+    val overlaps = rucksacks._1.intersect(rucksacks._2)
+    overlaps.head
+  }
+
+  def evaluateRucksacks2(rucksacks: List[String]): Char = {
+    val overlaps = rucksacks.head.intersect(rucksacks(1)).intersect(rucksacks(2))
+    overlaps(0)
+  }
 
   def readFile(): List[String] = {
     var filename = s"src/main/resources/input_day${day}"
@@ -20,19 +36,4 @@ object Day3 {
     list
   }
 
-  extension [T:ClassTag] (twoDimArr:Array[Array[T]]) {
-    def mirrored: Array[Array[T]] = {
-      var arr = Array.ofDim[T](twoDimArr.head.length, twoDimArr.length)
-      for (i <- twoDimArr.indices) {
-        for (j <- twoDimArr(i).indices){
-          arr(j)(i) = twoDimArr(i)(j)
-        }
-      }
-      arr
-    }
-
-    def print(twoDimesionalArr: Array[Array[T]]) : Unit = {
-      twoDimesionalArr.foreach(oneDimesionalArr => oneDimesionalArr.mkString(""))
-    }
-  }
 }
