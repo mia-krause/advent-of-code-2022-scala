fca713f9ff73d1ee784a285a975e73d424efc791
mia-krause <mia.elisabeth.krause+github@gmail.com>
2022-12-04 08:49:00 +0100

    day4 and empty input files

--- src/main/scala/adventofcode/Day4.scala
+++ src/main/scala/adventofcode/Day4.scala
@@ -1,10 +1,22 @@
 package adventofcode
 
-object Day4 extends AocBase[String] {
+import scala.collection.immutable.Set
+
+object Day4 extends AocBase[Int] {
   def main(args: Array[String]): Unit = {
-    part1("", (_: List[String]) => "")
-    part2("", (_: List[String]) => "")
+    part1(2, (input: List[String]) => toAssignedSections(input).map((a, b) => a.subsetOf(b) || b.subsetOf(a)).count(identity))
+    part2(4, (input: List[String]) => toAssignedSections(input).map((a, b) => a & b).count(_.nonEmpty))
   }
+
+  def toAssignedSections(input: List[String]): List[(Set[Int], Set[Int])] = {
+    input.map(_.split(',')).map(arr => (itemize(arr(0)), itemize(arr(1))))
+  }
+
+  def itemize(sectionWithMinus: String): Set[Int] = {
+    val pairs = sectionWithMinus.split('-')
+    Range.inclusive(pairs(0).toInt, pairs(1).toInt).toSet
+  }
+
 
 
 }
